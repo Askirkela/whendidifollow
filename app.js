6d22b0deb7fd9697706c38a096586d2ebc3f5f97
Askirkela <ldebene@gmail.com>
2015-10-01 19:33:20 +0200

    Added call to get the followers

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var app = angular.module('TwitchFollowing', []);
 
 app.controller('FollowsController', ['$scope', '$http', function(scope, http){
-	scope.total = 0;
+	scope.followsTotal = 0;
+	scope.followersTotal = 0;
+	/*
+	 * Send a HTTP request to the Twitch API.
+	 * Response format is a classic HTTP response (headers, data, etc...)
+	 * We store the response's data in our scope and update the total
+	 */
 	scope.getFollows = function(userName) {
-		console.log("Username : "+userName);
 		http.get('https://api.twitch.tv/kraken/users/'+userName+'/follows/channels?limit=100')
 		.then(function successCallback(response){
-			scope.follows = response.data;
-			scope.total = response.data._total;
-			console.log(scope.follows);
-		}, 
-			function errorCallback(response) {
-			console.log(response);
-		})
+			scope.followsData = response.data;
+			scope.followsTotal = response.data._total;
+		});
+		http.get('https://api.twitch.tv/kraken/channels/'+userName+'/follows?limit=100')
+		.then(function successCallback(response){
+			scope.followersData = response.data;
+			scope.followersTotal = response.data._total;
+			console.log(scope.followersData);
+		});
 	};
 }]);
